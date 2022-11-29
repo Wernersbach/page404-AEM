a76fb9d16d068fe3b545a0eff7241c524e33e2ba
Wernersbach <45134217+Wernersbach@users.noreply.github.com>
2022-11-28 22:37:28 -0300

    ajuste nos espaçamentos

--- ui.frontend/src/components/HandImage/index.js
+++ ui.frontend/src/components/HandImage/index.js
@@ -3,9 +3,7 @@ import './style.css';
 
 const HandImage = ({image = {}}) => {
   return(
-    <div className='hand-container-image'>
       <img className='main--image' src={image.src} alt="imagem" aria-hidden="true"/>
-    </div>
   )
 }
 
